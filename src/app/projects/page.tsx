0abf0ce36bcf81599581a07aeef071f6349386c8
Neil Patrick Villanueva <neilpatz16@gmail.com>
2024-12-03 21:00:27 +0800

    updated projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Card, CardContent } from "@/components/ui/card";
-import { Code2Icon } from "lucide-react";
+import { Code2Icon, Link2 } from "lucide-react";
 import { CgUnavailable } from "react-icons/cg";
 import Link from "next/link";
 import { motion } from "motion/react";
@@ -21,21 +21,27 @@ const projects = [
         technologyStack: ["React", "Next.js", "TypeScript", "Mongoose", "MongoDB", "Next-Auth.js"],
         description: "A web application developed for the MAPUA University's Center for Student Advising (CSA) to streamline and enhance the peer mentoring process. It features user authentication and authorization, automated schedule assignments, integrated communication tools, and comprehensive management functionalities.",
         source: "https://github.com/drxc00/mentor-match-public",
-        isSourcePublic: true
+        isSourcePublic: true,
+        isWebApp: true,
+        webLink: "https://www.mentormatch.online/"
     },
     {
         name: "SteamBud",
         technologyStack: ["Python", "Discord.py", "CheapSharkAPI"],
         description: "SteamBud is a Discord bot designed to facilitate the search for video game deals using the powerful CheapSharkAPI.",
         source: "https://github.com/drxc00/SteamBud-Bot",
-        isSourcePublic: true
+        isSourcePublic: true,
+        isWebApp: false,
+        webLink: ""
     },
     {
         name: "random_filipino_api",
         technologyStack: ["Python", "Flask", "Flask-RESTful", "SQLite", "SQLAlchemy"],
         description: "An API to generate random Filipino user data for testing and mock data creation. It provides endpoints for retrieving randomized names, addresses, phone numbers, and other personal details, with options to customize results by size and gender.",
         source: "https://github.com/drxc00/random_filipino_api",
-        isSourcePublic: true
+        isSourcePublic: true,
+        isWebApp: false,
+        webLink: ""
     },
 ];
 
@@ -82,6 +88,15 @@ export default function Projects() {
                                                             </TooltipContent>
                                                         </Tooltip>
                                                     </TooltipProvider>
+                                                    {project.isWebApp && (
+                                                        <div>
+                                                            <Link href={project.webLink} target="_blank">
+                                                                <div className="flex flex-col items-center rounded border p-2 bg-muted text-muted-foreground">
+                                                                    <Link2 className="w-3 h-3" />
+                                                                </div>
+                                                            </Link>
+                                                        </div>
+                                                    )}
                                                     <TextScramble textString={project.name} />
                                                 </div>
                                             </div>
@@ -89,10 +104,10 @@ export default function Projects() {
                                                 <div className="mt-5 gap-2 text-sm text-foreground flex flex-wrap w-full ">
                                                     {project.technologyStack.map((stack, index) => (
                                                         <motion.div
-                                                        key={index}
-                                                        initial={{ opacity: 0, x:10 }} // Start transparent and below
-                                                        animate={{ opacity: 1, x: 0 }} // Fade in and move to position
-                                                        transition={{ delay: index / (project.technologyStack.length + 1), duration: 0.3 }} // Same transition for all
+                                                            key={index}
+                                                            initial={{ opacity: 0, x: 10 }} // Start transparent and below
+                                                            animate={{ opacity: 1, x: 0 }} // Fade in and move to position
+                                                            transition={{ delay: index / (project.technologyStack.length + 1), duration: 0.3 }} // Same transition for all
                                                         >
                                                             <Badge
                                                                 className="border-border cursor-pointer bg-muted text-muted-foreground hover:bg-muted/90"
